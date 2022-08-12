4d1984f192d793431e746291f1277dfe63190795
SlaviSolakchiev <78353852+SlaviSolakchiev@users.noreply.github.com>
2022-08-12 17:05:41 +0300

    ScaffoldLoginAndRegisterMOdels

--- Web/FireAndIce.Web/Program.cs
+++ Web/FireAndIce.Web/Program.cs
@@ -1,4 +1,7 @@
-﻿namespace FireAndIce.Web
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using FireAndIce.Data;
+namespace FireAndIce.Web
 {
     using System.Reflection;
 
@@ -26,9 +29,17 @@
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
+                        var connectionString = builder.Configuration.GetConnectionString("ApplicationDbContextConnection") ?? throw new InvalidOperationException("Connection string 'ApplicationDbContextConnection' not found.");
+
+                                    builder.Services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(connectionString));
+
+                                                builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                .AddEntityFrameworkStores<ApplicationDbContext>();
             ConfigureServices(builder.Services, builder.Configuration);
             var app = builder.Build();
             Configure(app);
+                        app.UseAuthentication();;
             app.Run();
         }
 
