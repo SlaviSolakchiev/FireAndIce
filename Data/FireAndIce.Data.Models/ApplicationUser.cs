5fcc013887b72363ca935973f71ea47fec610679
SlaviSolakchiev <78353852+SlaviSolakchiev@users.noreply.github.com>
2022-08-18 15:49:56 +0300

    New db models

--- Data/FireAndIce.Data.Models/ApplicationUser.cs
+++ Data/FireAndIce.Data.Models/ApplicationUser.cs
@@ -16,6 +16,7 @@ namespace FireAndIce.Data.Models
             this.Roles = new HashSet<IdentityUserRole<string>>();
             this.Claims = new HashSet<IdentityUserClaim<string>>();
             this.Logins = new HashSet<IdentityUserLogin<string>>();
+            this.ServiceRequests = new HashSet<ServiceRequest>();
         }
 
         // Audit info
@@ -33,5 +34,7 @@ namespace FireAndIce.Data.Models
         public virtual ICollection<IdentityUserClaim<string>> Claims { get; set; }
 
         public virtual ICollection<IdentityUserLogin<string>> Logins { get; set; }
+
+        public virtual ICollection<ServiceRequest> ServiceRequests { get; set; }
     }
 }
